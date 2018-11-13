2d6056c80b5de101fe686a73a1a60352c5640258
Rob Brown <brown4homes@yahoo.com>
2018-11-13 10:52:36 -0800

    Installed Faker gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
+  before_action :common, :only => [:index, :team]
+
+  def common
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+  
+  end
 
   def index
+   
   end
 
   def team
+
   end
-  
+
 end
