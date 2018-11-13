9fa82253ac1879cb1ea8c74c772b493cdc7eb34a
Rob Brown <brown4homes@yahoo.com>
2018-11-13 11:13:22 -0800

    Added superhero actions and views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,9 @@ class HomeController < ApplicationController
   def common
     @hero_name = Faker::Superhero.name
     @hero_power = Faker::Superhero.power
-  
+    @corp_power = Faker::Company.bs
+    @corp_title = Faker::Job.title
+    @corp_power = @corp_power.titleize
   end
 
   def index
