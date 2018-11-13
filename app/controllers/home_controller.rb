f3103bd0a4b143daff72513abea3f0bd539c4c77
Rob Brown <brown4homes@yahoo.com>
2018-11-13 11:44:31 -0800

    Added random avatars and backgrounds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,26 @@ class HomeController < ApplicationController
     @corp_power = Faker::Company.bs
     @corp_title = Faker::Job.title
     @corp_power = @corp_power.titleize
+
+    @rand_num = rand(1..1000)
+    @set_number = rand(1..2)
+
+    if @set_number == 1
+      @set = "set1"
+    else
+      @set = "set2"
+    
+    end
+
+    @bg = rand(1..2)
+    if @bg == 1
+      @background = "bg1"
+    else
+      @background = "bg2"
+    end
+ 
+  @avatar = Faker::Avatar.image(@rand_num, "300x300", "png", @set,  @background)
+
   end
 
   def index
